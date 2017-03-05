58f3153e5699f6c6bc803157784eb80a8cdbdacb
blackcata <blackcata109@gmail.com>
2017-03-06 02:42:38 +0900

    Add mean and rms calculating subroutine

--- SEM_statistics.f90
+++ SEM_statistics.f90
@@ -11,14 +11,39 @@
         SUBROUTINE SEM_STAT
 
             USE SEM_module,                                                     &
-              ONLY : N, Ny, Nz, Nt, dt, SIGMA, V_b
+              ONLY : N, Ny, Nz, Nt, dt, time
 
             USE SEM_module,                                                     &
               ONLY : Y, Z, U, V, W, RS, SEM_EDDY, U_INLET, V_INLET, W_INLET,    &
-                     U_COMB, V_COMB, W_COMB
+                     U_COMB, V_COMB, W_COMB, U_c, U_pr, rms_pr
 
             IMPLICIT NONE
-            INTEGER :: j,k
+            INTEGER :: it,j,k,tt
+            REAL(KIND=8) :: U_tmp(3,1:Ny), rms_tmp(4,1:Ny)
+            tt = INT(time / dt)
 
-            
+            U_tmp(1:3,1:Ny)   = 0.0
+            rms_tmp(1:4,1:Ny) = 0.0
+
+            DO j = 1,Ny
+              DO k = 1,Nz
+                  U_tmp(1,j) = U_tmp(1,j) + U_COMB(j,k)
+                  U_tmp(2,j) = U_tmp(2,j) + V_COMB(j,k)
+                  U_tmp(3,j) = U_tmp(3,j) + W_COMB(j,k)
+
+                  rms_tmp(1,j) = rms_tmp(1,j) + (U_COMB(j,k) - U(j,k))**2
+                  rms_tmp(2,j) = rms_tmp(2,j) + (V_COMB(j,k) - V(j,k))**2
+                  rms_tmp(3,j) = rms_tmp(3,j) + (W_COMB(j,k) - W(j,k))**2
+                  rms_tmp(4,j) = rms_tmp(4,j) +                                 &
+                                 (U_COMB(j,k) - U(j,k))*(V_COMB(j,k) - V(j,k))
+              END DO
+
+              U_tmp(1:3,j) = U_tmp(1:3,j)/Nz
+              U_pr(1:3,j)  = ( U_pr(1:3,j) * (tt - 1) + U_tmp(1:3,j) )/tt
+
+              rms_tmp(1:4,j) = rms_tmp(1:4,j)/Nz
+              rms_pr(1:4,j) = ( rms_pr(1:4,j) * (tt - 1) + rms_tmp(1:4,j) )/tt
+
+            END DO
+
         END SUBROUTINE SEM_STAT
--- SEM_write.f90
+++ SEM_write.f90
@@ -9,15 +9,18 @@
 !------------------------------------------------------------------------------!
           SUBROUTINE OUTPUT
             USE SEM_module,                                                     &
-              ONLY : N, Ny, Nz, time, file_name, dir_name, path_name
+              ONLY : N, Ny, Nz, Nt, dt, time, file_name, dir_name, path_name
 
             USE SEM_module,                                                     &
               ONLY : Y, Z, U, V, W, RS, U_INLET, V_INLET, W_INLET, SEM_EDDY,    &
-                     U_COMB, V_COMB, W_COMB
+                     U_COMB, V_COMB, W_COMB, U_c, U_pr, rms_pr
 
               IMPLICIT NONE
               INTEGER :: it,j,k
-              REAL(KIND=8) :: time_sta, time_end
+              REAL(KIND=8) :: time_sta, time_end, U_mean(3,1:Ny), rms_mean(4,1:Ny)
+
+              U_mean(1:3,1:Ny)   = 0.0
+              rms_mean(1:4,1:Ny) = 0.0
 
               ! WRITE(*,*) '----------------------------------------------------'
               ! WRITE(*,*) '              WRITING PROCESS STARTED               '
@@ -31,7 +34,7 @@
               path_name = TRIM(dir_name)//TRIM(file_name)
 
               OPEN(100,FILE=path_name,FORM='FORMATTED',POSITION='APPEND')
-              WRITE(100,*) 'VARIABLES = X,Y,U_ins,V_ins,W_ins'
+              WRITE(100,*) 'VARIABLES = Z,Y,U_ins,V_ins,W_ins'
               WRITE(100,"(2(A,I3,2X))")' ZONE  I = ',Nz,' J = ',Ny
               WRITE(100,*) 'SOLUTIONTIME =',time
 
@@ -45,6 +48,59 @@
               END DO
               WRITE(100,*)
               CLOSE(100)
+
+              !----------------------------------------------------------------!
+              !                   Outputs for U mean profiles                  !
+              !----------------------------------------------------------------!
+              file_name = '/U_profiles.plt'
+              path_name = TRIM(dir_name)//TRIM(file_name)
+
+              IF ( INT(time/dt) == Nt) THEN
+                OPEN(100,FILE=path_name,FORM='FORMATTED',POSITION='APPEND')
+                WRITE(100,*)'VARIABLES = Y,U_ins,U_exac,V_ins,V_exac,W_ins,W_exac'
+
+                DO j = 1,Ny
+                  DO k = 1,Nz
+                    U_mean(1,j) = U_mean(1,j) + U(j,k)
+                    U_mean(2,j) = U_mean(2,j) + V(j,k)
+                    U_mean(3,j) = U_mean(3,j) + W(j,k)
+                  END DO
+                    U_mean(1,j) = U_mean(1,j)/Nz
+                    WRITE(100,"(7F15.9)") Y(j), U_pr(1,j), U_mean(1,j),         &
+                                                U_pr(2,j), U_mean(2,j),         &
+                                                U_pr(3,j), U_mean(3,j)
+                END DO
+                WRITE(100,*)
+                CLOSE(100)
+              END IF
+
+              !----------------------------------------------------------------!
+              !              Outputs for Reynolds stress profiles              !
+              !----------------------------------------------------------------!
+              file_name = '/RMS_profiles.plt'
+              path_name = TRIM(dir_name)//TRIM(file_name)
+
+              IF ( INT(time/dt) == Nt) THEN
+                OPEN(100,FILE=path_name,FORM='FORMATTED',POSITION='APPEND')
+                WRITE(100,*)'VARIABLES = Y,uu,uu_exac,vv,vv_exac,ww,ww_exac     &
+                                           uv,uv_exac'
+
+                DO j = 1,Ny
+                  DO k = 1,Nz
+                    rms_mean(1,j) = rms_mean(1,j) + RS(1,j,k)
+                    rms_mean(2,j) = rms_mean(2,j) + RS(2,j,k)
+                    rms_mean(3,j) = rms_mean(3,j) + RS(3,j,k)
+                    rms_mean(4,j) = rms_mean(4,j) + RS(4,j,k)
+                  END DO
+                    rms_mean(1:4,j) = rms_mean(1:4,j)/Nz
+                    WRITE(100,"(9F15.9)") Y(j), rms_pr(1,j), rms_mean(1,j),     &
+                                                rms_pr(2,j), rms_mean(2,j),     &
+                                                rms_pr(3,j), rms_mean(3,j),     &
+                                                rms_pr(4,j), rms_mean(4,j)
+                END DO
+                WRITE(100,*)
+                CLOSE(100)
+              END IF
 
               !----------------------------------------------------------------!
               !                   Outputs for Eddy posoitions                  !
