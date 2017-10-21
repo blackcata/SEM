5cb32827257b1d13c1b0e944f96fee22722fc603
blackcata <blackcata109@gmail.com>
2017-10-22 08:15:13 +0900

    Add normalization setting by using UT,TT,DELTA_NU

--- SEM_write.f90
+++ SEM_write.f90
@@ -23,35 +23,39 @@
 
               IMPLICIT NONE
               INTEGER :: it,j,k
-              REAL(KIND=8) :: U_mean(4,1:Ny), rms_mean(10,1:Ny)
+              REAL(KIND=8) :: U_mean(4,1:Ny), rms_mean(10,1:Ny), UT, TT, NUT
 
               U_mean(1:4,1:Ny)   = 0.0
               rms_mean(1:10,1:Ny) = 0.0
 
               dir_name = 'RESULT'
 
+              UT  = 5.5e-2
+              TT  = 6.58e-2
+              NUT = 6.06e-2
+
               !----------------------------------------------------------------!
               !                       Outputs for U Slice                      !
               !----------------------------------------------------------------!
-              ! file_name = '/U_ins.plt'
-              ! path_name = TRIM(dir_name)//TRIM(file_name)
-              !
-              ! OPEN(100,FILE=path_name,FORM='FORMATTED',POSITION='APPEND')
-              ! WRITE(100,*) 'VARIABLES = Z,Y,U_ins,V_ins,W_ins,T_ins'
-              ! WRITE(100,"(2(A,I3,2X))")' ZONE  I = ',Nz,' J = ',Ny
-              ! WRITE(100,*) 'SOLUTIONTIME =',time
-              !
-              ! DO j = 1,Ny
-              !   DO k = 1,Nz
-              !
-              !       WRITE(100,"(5F15.9)") Z(k),Y(j),                            &
-              !                             U_COMB(j,k),V_COMB(j,k),W_COMB(j,k),  &
-              !                             T_COMB(j,k)
-              !
-              !   END DO
-              ! END DO
-              ! WRITE(100,*)
-              ! CLOSE(100)
+            !  file_name = '/U_ins.plt'
+            !  path_name = TRIM(dir_name)//TRIM(file_name)
+            !
+            !  OPEN(100,FILE=path_name,FORM='FORMATTED',POSITION='APPEND')
+            !  WRITE(100,*) 'VARIABLES = Z,Y,U_ins,V_ins,W_ins,T_ins'
+            !  WRITE(100,"(2(A,I3,2X))")' ZONE  I = ',Nz,' J = ',Ny
+            !  WRITE(100,*) 'SOLUTIONTIME =',time
+            !
+            !  DO j = 1,Ny
+            !    DO k = 1,Nz
+            !
+            !        WRITE(100,"(5F15.9)") Z(k),Y(j),                            &
+            !                              U_COMB(j,k),V_COMB(j,k),W_COMB(j,k),  &
+            !                              T_COMB(j,k)
+            !
+            !    END DO
+            !  END DO
+            !  WRITE(100,*)
+            !  CLOSE(100)
 
               !----------------------------------------------------------------!
               !                   Outputs for U mean profiles                  !
@@ -71,10 +75,15 @@
                     U_mean(4,j) = U_mean(4,j) + T_READ(j,k)
                   END DO
                     U_mean(1:4,j) = U_mean(1:4,j)/Nz
-                    WRITE(100,"(9F15.9)") Y(j), U_pr(1,j), U_mean(1,j),         &
-                                                U_pr(2,j), U_mean(2,j),         &
-                                                U_pr(3,j), U_mean(3,j),         &
-                                                U_pr(4,j), U_mean(4,j)
+!                    WRITE(100,"(9F15.9)") Y(j), U_pr(1,j), U_mean(1,j),         &
+!                                                U_pr(2,j), U_mean(2,j),         &
+!                                                U_pr(3,j), U_mean(3,j),         &
+!                                                U_pr(4,j), U_mean(4,j)
+
+                    WRITE(100,"(9F15.9)") Y(j)/NUT, U_pr(1,j)/UT, U_mean(1,j)/UT,         &
+                                                U_pr(2,j)/UT, U_mean(2,j)/UT,         &
+                                                U_pr(3,j)/UT, U_mean(3,j)/UT,         &
+                                                U_pr(4,j)/TT, U_mean(4,j)/TT
                 END DO
                 WRITE(100,*)
                 CLOSE(100)
@@ -106,24 +115,34 @@
                     rms_mean(10,j) = rms_mean(10,j) + THS(4,j,k)
                   END DO
                     rms_mean(1:10,j) = rms_mean(1:10,j)/Nz
-                    WRITE(100,"(21F15.9)") Y(j), rms_pr(1,j), rms_mean(1,j),    &
-                                                 rms_pr(2,j), rms_mean(2,j),    &
-                                                 rms_pr(3,j), rms_mean(3,j),    &
-                                                 rms_pr(4,j), rms_mean(4,j),    &
-                                                 rms_pr(5,j), rms_mean(5,j),    &
-                                                 rms_pr(6,j), rms_mean(6,j),    &
-                                                 rms_pr(7,j), rms_mean(7,j),    &
-                                                 rms_pr(8,j), rms_mean(8,j),    &
-                                                 rms_pr(9,j), rms_mean(9,j),    &
-                                                 rms_pr(10,j), rms_mean(10,j)
+                  !  WRITE(100,"(21F15.9)") Y(j), rms_pr(1,j), rms_mean(1,j),    &
+                  !                               rms_pr(2,j), rms_mean(2,j),    &
+                  !                               rms_pr(3,j), rms_mean(3,j),    &
+                  !                               rms_pr(4,j), rms_mean(4,j),    &
+                  !                               rms_pr(5,j), rms_mean(5,j),    &
+                  !                               rms_pr(6,j), rms_mean(6,j),    &
+                  !                               rms_pr(7,j), rms_mean(7,j),    &
+                  !                               rms_pr(8,j), rms_mean(8,j),    &
+                  !                               rms_pr(9,j), rms_mean(9,j),    &
+                  !                               rms_pr(10,j), rms_mean(10,j)
+                    WRITE(100,"(21F15.9)") Y(j)/NUT, rms_pr(1,j)/UT**2, rms_mean(1,j)/UT**2,    &
+                                                 rms_pr(2,j)/UT**2, rms_mean(2,j)/UT**2,    &
+                                                 rms_pr(3,j)/UT**2, rms_mean(3,j)/UT**2,    &
+                                                 rms_pr(4,j)/TT**2, rms_mean(4,j)/TT**2,    &
+                                                 rms_pr(5,j)/UT**2, rms_mean(5,j)/UT**2,    &
+                                                 rms_pr(6,j)/UT**2, rms_mean(6,j)/UT**2,    &
+                                                 rms_pr(7,j)/UT**2, rms_mean(7,j)/UT**2,    &
+                                                 rms_pr(8,j)/(UT*TT), rms_mean(8,j)/(UT*TT),    &
+                                                 rms_pr(9,j)/(UT*TT), rms_mean(9,j)/(UT*TT),    &
+                                                 rms_pr(10,j)/(UT*TT), rms_mean(10,j)/(UT*TT)
                 END DO
                 WRITE(100,*)
                 CLOSE(100)
               END IF
 
-              ! !----------------------------------------------------------------!
-              ! !                   Outputs for Eddy posoitions                  !
-              ! !----------------------------------------------------------------!
+              !----------------------------------------------------------------!
+              !                   Outputs for Eddy posoitions                  !
+              !----------------------------------------------------------------!
               ! file_name = '/EDDY_POS.plt'
               ! path_name = TRIM(dir_name)//TRIM(file_name)
               !
